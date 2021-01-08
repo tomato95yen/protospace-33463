14fdde55a059091af5cd0e0824c3812eeba742d5
tomato95yen <74520325+tomato95yen@users.noreply.github.com>
2021-01-08 23:23:41 +0900

    誤りの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   validates :profile, presence: true
   validates :occupation, presence: true
   validates :position, presence: true
+  
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
